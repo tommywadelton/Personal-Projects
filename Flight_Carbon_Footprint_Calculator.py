2ca8ade7ab315ec96ff390b9b395425923d967c1
tommywadelton <143138951+tommywadelton@users.noreply.github.com>
2023-08-24 16:39:33 -0600

    Update Flight_Carbon_Footprint_Calculator.py
    This was a personal project as someone who wants to help the environment but also loves to travel. I wanted to know what I could do to offset my carbon footprint. So I looked up the calculation based on the average commercial airplane, and calculated the distance between each airport. This gives me a rough estimate on what I need to donate in order to offset that. teamtrees.org

--- Flight_Carbon_Footprint_Calculator.py
+++ Flight_Carbon_Footprint_Calculator.py
@@ -4,17 +4,19 @@ Created on Thu Aug 24 15:28:44 2023
 
 @author: twadel816
 """
-
+#import packages
 import pandas as pd
 from math import radians, sin, cos, sqrt, atan2
 
-df = pd.read_excel('C:\\Users\\twadel816\\Downloads\\US_Airport_Data.xlsx', sheet_name='Travel_to_Date_2')
-pd.set_option('display.max_columns', 12)
+#the excel sheet contains information on trips I have taken (starting/ending location, date, and the lat/long of each corresponding airport)
 
+df = pd.read_excel('C:\\Users\\twadel816\\Downloads\\US_Airport_Data.xlsx', sheet_name='Travel_to_Date_2')
 distances = []  # List to store distances
 
 for trip in df['Trip']:
     if df.iloc[int(trip) - 1]['Roundtrip'] == 'Y':
+        #calculate the distance between the two airports in km's
+        
         R = 6373.0
         lat1 = radians(df.iloc[int(trip) - 1]['Starting_Lat'])
         lon1 = radians(df.iloc[int(trip) - 1]['Starting_Long'])
@@ -28,6 +30,8 @@ for trip in df['Trip']:
         c = 2 * atan2(sqrt(a), sqrt(1 - a))
         
         distance = R * c
+        #multiplied by 2 for roundtrip
+        
         distance = round(distance, 0) * 2
         distances.append(distance)
     
@@ -59,6 +63,8 @@ for trip, (distance, start, end, date) in enumerate(zip(distances, df['Starting 
     
     formatted_date = date.strftime("%m/%d/%Y")
 
+    #prints the trip details, from where to where, on what date, totalling how many miles and the amount of trees/money it will take to donate to offset my carbon footprint based on this flight
+    
     print(f"My trip from {start} to {end} on {formatted_date}, {distance_miles} miles, produced {carbon_calc}kg of C02. I need to donate {trees_dollars} to offset this amount")
 
 
